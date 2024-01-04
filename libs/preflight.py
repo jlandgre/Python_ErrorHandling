79c94382349f8b5c06b248022141c1e2dc549b31
jlandgre <jdlandgrebe@icloud.com>
2024-01-04 12:02:57 -0500

    Validated CheckFilesProcedure - through 12:00 pm 1/4/24

--- libs/preflight.py
+++ libs/preflight.py
@@ -1,7 +1,7 @@
-#Version 1/3/24
-import pandas as pd
+#Version 1/4/24
 import inspect
 import os
+from openpyxl import load_workbook
 
 class CheckExcelFiles:
     def __init__(self, lst_files, lst_shts, errs):
@@ -9,40 +9,87 @@ class CheckExcelFiles:
         Initialize CheckExcelFiles with a list of file paths and a list of sheets.
         JDL 1/2/24
         """
-        self.lst_files = lst_files
-        self.lst_shts = lst_shts
+        self.lst_files = lst_files # list of file paths to check
+        self.lst_shts = lst_shts # list of lists of sheets
 
+        self.wb = None # Current Excel workbook object
+        self.IsWbErr = False # Flag if any errors during procedure
+
+        # ErrorHandle instance and atts - Errors are treated as non-fatal warnings
         self.errs = errs
         self.errs.IsWarning = True
         self.errs.IsPrint = False
 
-    def ExcelFileExists(self, errs):
+    def CheckFilesProcedure(self):
+        """
+        Procedure to check specified Excel files and sheets
+        JDL 1/4/24
+        """
+        for idx in range(len(self.lst_files)):
+            self.IsWbErr = False
+
+            # Check that file exists and can be opened; set self.wb object
+            if not self.ExcelFileExists(idx): continue
+            if not self.ExcelFileOpens(idx): continue
+
+            # If valid Excel file, check if specified sheets are present
+            if self.AllSheetsExist(idx): self.wb.Close(False)
+
+    def ExcelFileExists(self, idx):
         """
         Check if each Excel file exists and if each sheet exists in the file.
-        JDL 1/3/24
+        JDL 1/4/24
         """
-        self.errs.Locn = current_fn()
+        fpath = self.lst_files[idx]
+        if self.errs.is_fail(not os.path.exists(fpath), 1, '\n ' + fpath + '\n'):
+            self.IsWbErr = True
+            self.errs.RecordErr()
+            return False
+        return True
 
-        # Iterate over each file in the list
-        for fpath in self.lst_files:
-            f = SplitPath(fpath)[1]
+    def ExcelFileOpens(self, idx):
+        """
+        Check if each Excel file exists and if each sheet exists in the file.
+        JDL 1/4/24
+        """
+        fpath = self.lst_files[idx]
+        try:
+            self.wb = load_workbook(filename=fpath)
+            return True
+        except Exception as e:
+            self.IsWbErr = True
+            self.errs.is_fail(True, 2, '\n ' + fpath + '\n')
+            self.errs.RecordErr()
+            return False
 
-            # Check if the file exists
-            if self.errs.is_fail(not os.path.exists(fpath), 1, f):
-                self.errs.RecordErr()
+    def AllSheetsExist(self, idx):
+        """
+        Iteratively check if specified sheets exist in self.wb
+        JDL 1/4/24
+        """
+        for sheet_name in self.lst_shts[idx]:
+            if not self.SheetExists(idx, sheet_name): break
 
-        # Print the accumulated error messages
-        if len(errs.Msgs_Accum) > 0: print('\n\n', errs.Msgs_Accum + '\n')
+    def SheetExists(self, idx, sheet_name):
+        """
+        Check if specified sheet exists in self.wb
+        JDL 1/4/24
+        """
+        #Set the path and errParam string for current file and sheet
+        fpath = self.lst_files[idx]
+        errParam = '\n Missing: ' + sheet_name + ' in ' + fpath + '\n'
 
-
-def SplitPath(filepath):
-    """
-    Split a file path into directory path and file name.
-    JDL 1/3/24
-    """
-    path, f = os.path.split(filepath)
-    return path, f
-
+        #Check if sheet exists in self.wb
+        is_sht_exists = sheet_name in self.wb.sheetnames
+        if self.errs.is_fail(not is_sht_exists, 3, errParam):
+            self.IsWbErr = True
+            self.errs.RecordErr()
+            return False
+        return True
 
 def current_fn():
+    """
+    Return the name of the current function
+    JDL 1/4/24
+    """
     return inspect.currentframe().f_back.f_code.co_name
--- tests/test_preflight.py
+++ tests/test_preflight.py
@@ -28,55 +28,172 @@ def errs():
 def df_errs_test():
     data = """
     iCode,Class,Locn,Msg_String
-    100,CheckExcelFiles,ExcelFileExists,Base
-    101,CheckExcelFiles,ExcelFileExists,ERROR: Input file not found
+    100,CheckExcelFiles,CheckFilesProcedure,Base
+    101,CheckExcelFiles,CheckFilesProcedure,ERROR: Input file not found
+    102,CheckExcelFiles,CheckFilesProcedure,ERROR: Input file not a valid Excel file
+    103,CheckExcelFiles,CheckFilesProcedure,ERROR: Required input file sheet not found
     """
     return pd.read_csv(StringIO(data), skipinitialspace=True)
 
+@pytest.fixture
+def check_files(errs, df_errs_test):
+    errs.df_errs = df_errs_test
+    errs.Locn = 'CheckFilesProcedure'
+    lst_files, lst_shts = ['../tests/test_mockup.xlsx'], [['']]
+    return CheckExcelFiles(lst_files, lst_shts, errs)
 """
 =========================================================================
-Tests of CheckExcelFile class methods
+Tests of CheckExcelFiles.CheckFilesProcedure() methods
+
+These tests use the mockup Excel file, test_mockup.xlsx and a dummy
+non-Excel file, dummy_file.docx in the tests directory. The tests use
+a virtual import of df_errs_test to construct the error code df that
+would typically be imported from ErrorCodes.xlsx. It would reside in
+the libs subfolder with error_handling.py and preflight.py files.
 =========================================================================
 """
-def test_ExcelFileExists1(errs, df_errs_test):
+def test_CheckFilesProcedure1(check_files):
+    """
+    Test the CheckFilesProcedure method of the CheckExcelFiles class
+    (1) Check of case where there are no errors
+    """
+    check_files.lst_shts = [['first_sheet', 'second_sheet']]
+    check_files.CheckFilesProcedure()
+    assert check_files.IsWbErr == False
+    assert len(check_files.errs.Msgs_Accum) == 0
+
+def test_CheckFilesProcedure2(check_files):
+    """
+    Test the CheckFilesProcedure method of the CheckExcelFiles class 
+    (2) non-existent file
+    """
+    check_files.lst_files = ['xxxx.xlsx']
+    check_files.CheckFilesProcedure()
+    assert check_files.IsWbErr == True
+    assert check_files.errs.Msgs_Accum[0:27] == 'ERROR: Input file not found'
+
+
+def test_CheckFilesProcedure3(check_files):
+    """
+    Test the CheckFilesProcedure method of the CheckExcelFiles class
+    (3) non-Excel file
+    """
+    check_files.lst_files = ['../tests/dummy_file.docx']
+    check_files.CheckFilesProcedure()
+    assert check_files.IsWbErr == True
+    assert check_files.errs.Msgs_Accum[0:27] == 'ERROR: Input file not a val'
+
+def test_CheckFilesProcedure4(check_files):
+    """
+    Test the CheckFilesProcedure method of the CheckExcelFiles class
+    (4) non-existent sheet
+    """
+    check_files.lst_shts = [['first_sheet', 'x_sheet', 'second_sheet']]
+    check_files.CheckFilesProcedure()
+    assert check_files.IsWbErr == True
+    assert check_files.errs.Msgs_Accum[0:32] == 'ERROR: Required input file sheet'
+
+    # Print accumulated error messages
+    #msgs = check_files.errs.Msgs_Accum
+    #if len(msgs) > 0: print('\n\n', msgs + '\n')
+
+def test_ExcelFileExists1(check_files):
     """
     Check if each Excel file exists and can be opened
     JDL 1/3/24
     """
-    errs.df_errs = df_errs_test
 
     # instance CheckExcelFiles and check for file presence
-    lst_files = ['../tests/test_mockup.xlsx']
-    lst_shts = [['']]
-    check_files = CheckExcelFiles(lst_files, lst_shts, errs)
-    check_files.ExcelFileExists(errs)
+    check_files.ExcelFileExists(idx=0)
 
     # Check that no error was recorded
-    assert check_files.errs.IsErr == False
+    assert check_files.IsWbErr == False
+    assert len(check_files.errs.Msgs_Accum) == 0
 
-def test_ExcelFileExists2(errs, df_errs_test, capfd):
+def test_ExcelFileExists2(check_files):
     """
     Check for file that does not exist
     JDL 1/3/24
     """
-    errs.df_errs = df_errs_test
-
     # instance CheckExcelFiles and check for file presence
-    lst_files = ['xxx.xlsx']
-    lst_shts = [['']]
-    check_files = CheckExcelFiles(lst_files, lst_shts, errs)
-    check_files.ExcelFileExists(errs)
-
-    # Capture the output
-    out, err = capfd.readouterr()
+    check_files.lst_files = ['xxx.xlsx']
+    check_files.ExcelFileExists(idx=0)
 
     # Get the expected error message for iCode 101
-    expected_msg = '\n\n ERROR: Input file not found: xxx.xlsx\n\n' 
-    assert expected_msg == out
+    expected_msg = 'ERROR: Input file not found: \n '\
+                    + check_files.lst_files[0] + '\n'
+    assert check_files.errs.Msgs_Accum == expected_msg
 
-def test_SplitPath():
-    path, f = preflight.SplitPath('../tests/test_mockup.xlsx')
+def test_ExcelFileOpens1(check_files):
+    """
+    Check if Excel file opens successfully
+    JDL 1/4/24
+    """
+    # Check file opens for valid Excel file
+    check_files.ExcelFileOpens(idx=0)
+    assert check_files.IsWbErr == False
+    assert len(check_files.errs.Msgs_Accum) == 0
 
-    # Check that the directory path and file name are correct
-    assert path == '../tests'
-    assert f == 'test_mockup.xlsx'
+def test_ExcelFileOpens2(check_files):
+    """
+    Check attempt to open non-Excel file
+    JDL 1/4/24
+    """
+    # check non-Excel test file
+    check_files.lst_files = ['../tests/dummy_file.docx']
+    check_files.ExcelFileOpens(idx=0)
+
+    assert check_files.IsWbErr == True
+    expected_msg = 'ERROR: Input file not a valid Excel file: \n '\
+    + check_files.lst_files[0] + '\n' 
+    assert check_files.errs.Msgs_Accum == expected_msg
+
+def test_AllSheetsExist1(check_files):
+    """
+    Test if all specified sheets exist in the Excel workbook
+    JDL 1/4/24
+    """
+
+    # Set the wb object by running check that file opens
+    idx = 0
+    check_files.ExcelFileOpens(idx)
+    check_files.lst_shts = [['first_sheet', 'second_sheet']]
+
+    check_files.AllSheetsExist(idx)
+    assert check_files.IsWbErr == False
+
+def test_AllSheetsExist2(check_files):
+    """
+    Test that non-existent sheet triggers error
+    """
+    idx = 0
+    check_files.ExcelFileOpens(idx)
+    check_files.lst_shts = [['first_sheet', 'x_sheet', 'second_sheet']]
+    check_files.AllSheetsExist(idx)
+
+    assert check_files.IsWbErr == True
+    expected_msg ='ERROR: Required input file sheet not found: \n '\
+                    'Missing: x_sheet in ' + check_files.lst_files[idx] + '\n'
+    assert check_files.errs.Msgs_Accum == expected_msg
+
+def test_SheetExists(check_files):
+    """
+    Check if specified sheet exists in Excel workbook
+    JDL 1/4/24
+    """
+    # Set the wb object by running FileOpens check
+    idx = 0
+    check_files.ExcelFileOpens(idx)
+
+    # Two sheets that exist 
+    assert check_files.SheetExists(idx, 'first_sheet') == True
+    assert check_files.SheetExists(idx, 'second_sheet') == True
+    assert check_files.IsWbErr == False
+    assert len(check_files.errs.Msgs_Accum) == 0
+
+    # One sheet that does not exist
+    assert check_files.SheetExists(idx, 'third_sheet') == False
+    assert check_files.IsWbErr == True
+    expected_msg ='ERROR: Required input file sheet not found: \n '\
+                    'Missing: third_sheet in ' + check_files.lst_files[0] + '\n'
+    assert check_files.errs.Msgs_Accum == expected_msg
